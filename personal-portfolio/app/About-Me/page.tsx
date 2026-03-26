bb3bfabab66d6a6683a51e454cdd71ebec2bdb8c
Niamh Wilson <niamhlwilson@gmail.com>
2026-03-26 20:25:35 +0800

    remove commented code

--- personal-portfolio/app/About-Me/page.tsx
+++ personal-portfolio/app/About-Me/page.tsx
@@ -5,8 +5,5 @@ export function AboutMe() {
                 <h1>Hello, my name is Niamh Wilson, and I'm a developer and maker</h1>
             </header>
         </div>
-        // <header>
-        //     <h1>Hello, I'm a<br><em>developer</em> &amp;<br>maker.</h1>
-        // </header>
     )
 }
