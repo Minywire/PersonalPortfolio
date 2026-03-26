74f396caeff02bf4728c4bf05cdc18fb07168bd5
Niamh Wilson <niamhlwilson@gmail.com>
2026-03-26 19:51:11 +0800

    modify navbar component and file organisation to change url from /Home to /

--- personal-portfolio/app/_components/navbar.tsx
+++ personal-portfolio/app/_components/navbar.tsx
@@ -17,11 +17,11 @@ export function Navbar() {
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
+            {navItems.map(({ name, href }) => {
               return (
                 <Link
-                  key={path}
-                  href={path}
+                  key={href}
+                  href={href}
                   className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
