404a9f35f906cab1d63001e43f4f67c8654acea7
Niamh Wilson <niamhlwilson@gmail.com>
2026-03-26 20:24:55 +0800

    add header

---
+++ personal-portfolio/app/About-Me/page.tsx
@@ -0,0 +1,12 @@
+export function AboutMe() {
+    return (
+        <div>
+            <header>
+                <h1>Hello, my name is Niamh Wilson, and I'm a developer and maker</h1>
+            </header>
+        </div>
+        // <header>
+        //     <h1>Hello, I'm a<br><em>developer</em> &amp;<br>maker.</h1>
+        // </header>
+    )
+}
