9fc2cf0cdd00f738008609f7fa190c2545b13620
Niamh Wilson <niamhlwilson@gmail.com>
2026-03-26 20:31:26 +0800

    add some basic info about me

---
+++ personal-portfolio/app/about/page.tsx
@@ -0,0 +1,17 @@
+export default function AboutMe() {
+    return (
+        <div>
+            <header>
+                <h1>Hello, my name is Niamh Wilson, and I'm a developer and maker</h1>
+                <p>Graduate of Murdoch University · Games technologist · Community moderator · Textile artist</p>
+                <div>
+                    <span>Perth, WA</span>
+                    <span >Class of 2025</span>
+                    <span>B.I.T · Games Technology</span>
+                    <span>B.I.T · Computer Science</span>
+                </div>
+            </header>
+            
+        </div>
+    )
+}
