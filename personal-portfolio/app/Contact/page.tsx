fd04e6cc5350425e179a73c4bd2e5eddb4aaba4b
Niamh Wilson <niamhlwilson@gmail.com>
2026-03-27 15:22:34 +0800

    add basic boilerplate to other pages

---
+++ personal-portfolio/app/Contact/page.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+    return (
+        <div>
+            <header>
+                <h1>Contact Me</h1>
+            </header>
+            <main>
+                <p>You can reach me at: <a href="mailto:niamhlwilson@gmail.com" /></p>
+                <p>Or find me on LinkedIn: <a href="https://www.linkedin.com/in/niamh-wilson/" /></p>
+            </main>
+        </div>
+    )
+}
